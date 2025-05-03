d8c0d2b44c9578182ef619f2e2029d570ead10f3
lesaintcode <rjunqueira@tulane.edu>
2025-05-02 21:33:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def simple_work_calc(n, a, b):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	if n <= 1:
+        	return 1
+    	else:
+        	return a * simple_work_calc(n // b, a, b) + n
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -32,8 +34,10 @@ def work_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	if n <= 1:
+		return 1
+	else:
+        	return a * work_calc(n // b, a, b, f) + f(n)
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -47,9 +51,10 @@ def span_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
-
+	if n <= 1:
+        	return 1
+    	else:
+        	return span_calc(n // b, a, b, f) + f(n)
 
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
